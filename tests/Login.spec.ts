6fb53300cd63d1ac0e13ef7f5f23820f5732fc71
user <sergei.zahharov1@gmail.com>
2025-02-26 13:15:06 +0200

    HW-16 |  final correction

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -2,14 +2,15 @@ import {expect, test} from "@playwright/test";
 import {LoginPage} from "../pages/login.page";
 import {faker} from "@faker-js/faker";
 
+let loginPage: LoginPage
+
 test.describe("Simple Login tests", async () => {
 test.beforeEach(async ({page})=>{
-    const loginPage = new LoginPage(page);
+    loginPage = new LoginPage(page);
     await loginPage.openLoginPage();
 })
 
-    test('Login with short login and password, should show validation error and signin button is disabled', async ({page}) => {
-        const loginPage = new LoginPage(page);
+    test('Login with short login and password, should show validation error and signin button is disabled', async () => {
         await loginPage.usernameField.fill(faker.lorem.word(1));
         await expect(loginPage.usernameInputError).toBeVisible();
         await loginPage.passwordField.fill(faker.lorem.word(7));
@@ -17,16 +18,13 @@ test.beforeEach(async ({page})=>{
         await expect(loginPage.signInButton).toBeDisabled();
     })
 
-    test('Login with empty login and password ', async ({page}) => {
-        const loginPage = new LoginPage(page);
+    test('Login with empty login and password ', async () => {
         await loginPage.usernameField.fill(" ");
         await loginPage.passwordField.fill(" ");
         await expect(loginPage.signInButton).toBeDisabled();
     })
 
-    test("Login with incorrect credentials, verify pop-up message appears", async ({page}) => {
-        const loginPage = new LoginPage(page);
-        await loginPage.openLoginPage();
+    test("Login with incorrect credentials, verify pop-up message appears", async () => {
         await loginPage.login(faker.internet.email(), faker.internet.password())
         await expect(loginPage.incorrectCredentialsPopUp).toContainText("Incorrect credentials");
     })
